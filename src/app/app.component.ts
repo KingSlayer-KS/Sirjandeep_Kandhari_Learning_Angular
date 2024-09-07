7d284ec96c1f5234ff7963df5ae2bac7e86c1451
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2024-09-07 01:46:31 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'a1';
+  name: string = 'Sirjan';
+  sem: number = 3;
 }
