fb94673d72376bd9d8b1efc8101bb28ca229f716
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2024-09-26 23:52:40 -0400

    content list array added content list component

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from '../models/item.model';
 
 @Component({
   selector: 'app-content-list',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './content-list.component.css'
 })
 export class ContentListComponent {
-
+  contentList: Item[] = [
+    { id: 1, name: 'Macbook', description: 'High-end performece laptop apple', price: 1500, inStock: true, category: 'Electronics' },
+    { id: 2, name: 'iphone', description: 'Latest model smartphone apple', price: 1000, category: 'Electronics' },
+    { id: 3, name: 'airpods', description: 'Noise-cancelling headphones apple', price: 200, inStock: false, category: 'Accessories' },
+    { id: 4, name: 'ipad', description: 'tablet apple', price: 250, inStock: true, category: 'Furniture' },
+    { id: 5, name: 'chair', description: 'Standing desk', price: 300, category: 'Furniture' },
+    { id: 6, name: 'Monitor', description: '4K monitor', price: 400, inStock: true, category: 'Electronics' }
+  ];
 }
