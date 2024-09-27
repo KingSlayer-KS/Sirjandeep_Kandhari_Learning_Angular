abfcab59caa29abfec5635a47376b04864577574
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2024-09-26 23:50:26 -0400

    ContentList component imported to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Item } from './models/item.model';
+import { ContentListComponent } from "./content-list/content-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
