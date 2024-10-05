29aafb82e7ebcff64dcccd91c77ae722845189b5
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2024-10-04 23:32:49 -0400

    added debendency ingection

--- src/app/content-list/content-list-service.service.ts
+++ src/app/content-list/content-list-service.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Item } from '../models/item.model';
+import { contentList } from '../data/items'; // Importing mock data
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ContentListServiceService {
-
-  constructor() { }
+export class ContentService {
+  getContentList(): Observable<Item[]> {
+    return of(contentList);
+  }
 }
--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; // Import OnInit to use lifecycle hooks
 import { Item } from '../models/item.model';
+import { ContentService } from './content-list-service.service';  // Import ContentService
 import { ContentListItemComponent } from "../content-list-item/content-list-item.component";
 
 @Component({
@@ -7,15 +8,16 @@ import { ContentListItemComponent } from "../content-list-item/content-list-item
   standalone: true,
   imports: [ContentListItemComponent],
   templateUrl: './content-list.component.html',
-  styleUrl: './content-list.component.css'
+  styleUrls: ['./content-list.component.css']
 })
-export class ContentListComponent {
-  contentList: Item[] = [
-    { id: 1, name: 'Macbook', description: 'High-end performece laptop apple', price: 1500, inStock: true, category: 'Electronics' },
-    { id: 2, name: 'iphone', description: 'Latest model smartphone apple', price: 1000, category: 'Electronics' },
-    { id: 3, name: 'airpods', description: 'Noise-cancelling headphones apple', price: 200, inStock: false, category: 'Accessories' },
-    { id: 4, name: 'ipad', description: 'tablet apple', price: 250, inStock: true, category: 'Furniture' },
-    { id: 5, name: 'chair', description: 'Standing desk', price: 300, category: 'Furniture' },
-    { id: 6, name: 'Monitor', description: '4K monitor', price: 400, inStock: true, category: 'Electronics' }
-  ];
+export class ContentListComponent implements OnInit {
+  contentList: Item[] = [];  
+
+  constructor(private contentService: ContentService) {}
+
+  ngOnInit(): void {
+    this.contentService.getContentList().subscribe(data => {
+      this.contentList = data; 
+    });
+  }
 }
