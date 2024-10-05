99c3419d48864bca009791e564c8b987967849ad
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2024-10-04 23:40:27 -0400

    added methos in services

--- src/app/content-list/content-list-service.service.ts
+++ src/app/content-list/content-list-service.service.ts
@@ -7,7 +7,34 @@ import { contentList } from '../data/items'; // Importing mock data
   providedIn: 'root',
 })
 export class ContentService {
+
   getContentList(): Observable<Item[]> {
     return of(contentList);
+  }
+
+  getContentById(id: number): Observable<Item | undefined> {
+    const foundItem = contentList.find(item => item.id === id);
+    return of(foundItem);   }
+
+  addContent(newItem: Item): Observable<Item[]> {
+    contentList.push(newItem);  
+    return of(contentList);  
+  }
+
+  updateContent(updatedItem: Item): Observable<Item[]> {
+    const index = contentList.findIndex(item => item.id === updatedItem.id);
+    if (index !== -1) {
+      contentList[index] = updatedItem; 
+    }
+    return of(contentList);  
+  }
+
+  deleteContent(id: number): Observable<Item | undefined> {
+    const index = contentList.findIndex(item => item.id === id);
+    if (index !== -1) {
+      const removedItem = contentList.splice(index, 1)[0];  
+      return of(removedItem); 
+    }
+    return of(undefined); 
   }
 }
