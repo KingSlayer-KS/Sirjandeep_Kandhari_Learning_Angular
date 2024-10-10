3cfb842a6ad78eca2c9c64be09cf9e0725665432
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2024-10-09 21:41:36 -0400

    added routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {ContentListComponent} from "./app/content-list/content-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path:'', redirectTo: '/product', pathMatch: 'full'}, //default route
+  { path: 'product', component: ContentListComponent },
+];
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
