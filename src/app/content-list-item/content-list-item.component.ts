dff2b89fb8ba70f04ec174616b83dab9c8f6d486
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2024-11-19 00:48:10 -0500

    custom pipe for full profuct name

--- src/app/content-list-item/content-list-item.component.ts
+++ src/app/content-list-item/content-list-item.component.ts
@@ -2,12 +2,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Item } from '../models/item.model';
 import { RouterLink } from '@angular/router';
 import { UpperCasePipe,CurrencyPipe, DatePipe } from '@angular/common';
+import { ProductFullNamePipe } from '../pipes/product-full-name.pipe';
 
 
 @Component({
   selector: 'app-content-list-item',
   standalone: true,
-  imports: [RouterLink,UpperCasePipe,CurrencyPipe,DatePipe],
+  imports: [RouterLink,UpperCasePipe,CurrencyPipe,DatePipe,ProductFullNamePipe],
   templateUrl: './content-list-item.component.html',
   styleUrls: ['./content-list-item.component.css']
 })
--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -6,5 +6,5 @@ export interface Item {
     inStock?: boolean;  
     category: string;
     img?:string;
-    listed?:Date
+    company?:string
   }
