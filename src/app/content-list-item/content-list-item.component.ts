f83cf5711ddae30b965cce04261219715ebdcd15
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2024-11-21 19:15:25 -0500

    Add HoverHighlight directive

--- src/app/content-list-item/content-list-item.component.ts
+++ src/app/content-list-item/content-list-item.component.ts
@@ -3,12 +3,12 @@ import { Item } from '../models/item.model';
 import { RouterLink } from '@angular/router';
 import { UpperCasePipe,CurrencyPipe, DatePipe } from '@angular/common';
 import { ProductFullNamePipe } from '../pipes/product-full-name.pipe';
-
+import { HoverHighlightDirective } from '../directives/hover-highlight.directive';
 
 @Component({
   selector: 'app-content-list-item',
   standalone: true,
-  imports: [RouterLink,UpperCasePipe,CurrencyPipe,DatePipe,ProductFullNamePipe],
+  imports: [RouterLink,UpperCasePipe,CurrencyPipe,DatePipe,ProductFullNamePipe,HoverHighlightDirective],
   templateUrl: './content-list-item.component.html',
   styleUrls: ['./content-list-item.component.css']
 })
---
+++ src/app/directives/hover-highlight.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHoverHighlight]',
+  standalone: true,
+})
+export class HoverHighlightDirective {
+  @Input('appHoverHighlight') highlightColor: string = 'yellow';
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
